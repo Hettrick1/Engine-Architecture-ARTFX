6db8ed80991f24ac6519add866998eb24a495eef
Evann Jacquot <evann.jacquot@artfx.fr>
2024-11-12 14:44:45 +0100

    [add] game class

---
+++ Engine-ARTFX/Window.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SDL.h>
+#include "Vector2D.h"
+#include "Log.h"
+
+class Window
+{
+public :
+	Window(int width = 800, int height = 700);
+	Window(const Window&) = delete;
+	void operator = (const Window&) = delete;
+
+	Vector2D GetDimensions() const;
+
+	SDL_Window* GetSdlWindow() const;
+
+	bool Open();
+	void Close();
+
+private:
+	SDL_Window* mSdlWindow;
+	Vector2D mDimensions;
+};
+
