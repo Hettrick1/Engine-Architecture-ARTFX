17f109341e650519d180ecdcb348db57fab44e93
Evann Jacquot <evann.jacquot@artfx.fr>
2024-11-12 15:19:10 +0100

    [add] Pong class

---
+++ Engine-ARTFX/Pong.cpp
@@ -0,0 +1,27 @@
+#include "Pong.h"
+
+
+Pong::Pong() : Scene()
+{
+}
+
+void Pong::Start(Renderer* renderer) 
+{
+	Scene::Start(renderer);
+}
+
+void Pong::Update()
+{
+}
+
+void Pong::Render()
+{
+}
+
+void Pong::OnInput(SDL_Event)
+{
+}
+
+void Pong::Close()
+{
+}
---
+++ Engine-ARTFX/Pong.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Scene.h"
+class Pong : public Scene
+{
+public:
+	Pong();
+	void Start(Renderer* renderer) override;
+	void Update() override;
+	void Render() override;
+	void OnInput(SDL_Event) override;
+	void Close() override;
+private:
+
+};
+
