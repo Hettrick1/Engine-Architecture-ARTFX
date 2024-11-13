fadbc1d802d077642532f1c88e16d62904131e25
Hettrick1 <evann.jacquot@gmail.com>
2024-11-13 20:43:57 +0100

    [refactor] box collider detection

--- Engine-ARTFX/Core/Engine/Actor Components/Colliders/BoxCollider2D.h
+++ Engine-ARTFX/Core/Engine/Actor Components/Colliders/BoxCollider2D.h
@@ -7,21 +7,25 @@ class BoxCollider2D : public Collider
 {
 public:
 	BoxCollider2D() = delete;
-	BoxCollider2D(Vector2D position, Vector2D size);
+	BoxCollider2D(Actor* owner, int updateOder, Vector2D position, Vector2D size);
 	~BoxCollider2D();
 	void OnStart() override;
 	void Update() override;
 	void OnEnd() override;
 
 public:
-	void CheckCollision() override;
+	void CheckCollisionWith(Collider* other) override;
 	void OnCollision() override;
+
+public:
+	bool CheckCollisionWithBox(BoxCollider2D* other);
 
 	/*void OnCollisionEnter(); //Need to be implemented
 	void OnCollisionQuit();
 	void OnTriggerEnter();
 	void OnTriggerStay();
 	void OnTriggerQuit();*/
+public:
 
 private:
 	Vector2D mPosition;
