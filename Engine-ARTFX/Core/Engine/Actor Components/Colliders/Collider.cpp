d58594974fa7b4ed9840563bfb46a2d68589e72b
Hettrick1 <evann.jacquot@gmail.com>
2024-11-13 20:44:15 +0100

    [refactor] collider detection system parent class

--- Engine-ARTFX/Core/Engine/Actor Components/Colliders/Collider.cpp
+++ Engine-ARTFX/Core/Engine/Actor Components/Colliders/Collider.cpp
@@ -1,1 +1,29 @@
 #include "Collider.h"
+
+Collider::Collider(Actor* owner, int updateOder) : Component(owner, updateOder)
+{
+}
+
+Collider::~Collider()
+{
+}
+
+void Collider::OnStart()
+{
+}
+
+void Collider::Update()
+{
+}
+
+void Collider::OnEnd()
+{
+}
+
+void Collider::CheckCollisionWith(Collider* other)
+{
+}
+
+void Collider::OnCollision()
+{
+}
--- Engine-ARTFX/Core/Engine/Actor Components/Colliders/Collider.h
+++ Engine-ARTFX/Core/Engine/Actor Components/Colliders/Collider.h
@@ -5,13 +5,14 @@
 class Collider : public Component
 {
 public:
-	Collider();
+	Collider() = delete;
+	Collider(Actor* owner, int updateOder);
 	~Collider();
 	virtual void OnStart();
 	virtual void Update();
 	virtual void OnEnd();
 public:
-	virtual void CheckCollision();
+	virtual void CheckCollisionWith(Collider* other);
 	virtual void OnCollision();
 	/*virtual void OnCollisionEnter(); //Need to be implemented
 	virtual void OnCollisionQuit();
