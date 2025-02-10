fd5e12a217b287de406b13687ba127259c4f9db5
Evann Jacquot <evann.jacquot@artfx.fr>
2025-02-10 18:00:17 +0100

    [add] texture class

---
+++ Engine-ARTFX/Core/Engine/Graphics/Texture.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include "SDL.h"
+#include "Renderer.h"
+#include "..\..\Maths\Vector2D.h"
+
+class Texture
+{
+public:
+	Texture();
+	~Texture();
+	bool Load(Renderer& renderer, const std::string& filename);
+	void Unload();
+	void UpdateInfo(int& width, int& height);
+	Vector2D GetTextureSize();
+
+private:
+	std::string mFilePath;
+	SDL_Texture* mSdlTexture;
+	int mWidth, mHeight;
+};
