5e91ea3c3da39661e09272f50a528f584d98b0e3
Evann Jacquot <evann.jacquot@artfx.fr>
2025-02-11 11:54:12 +0100

    [add] sprite component

---
+++ Engine-ARTFX/Core/Engine/Actor Components/SpriteComponent.cpp
@@ -0,0 +1,27 @@
+#include "SpriteComponent.h"
+
+#include "Actor.h"
+#include "../../Scenes/Scene.h"
+
+SpriteComponent::SpriteComponent(Actor* pOwner, Texture& pTexture, int pDrawOrder)
+	: Component(pOwner), mTexture(pTexture), mDrawOrder(pDrawOrder), mTexWidth(pTexture.GetTextureSize().x), mTexHeight(pTexture.GetTextureSize().y)
+{
+	mOwner->GetScene().GetRenderer()->AddSprite(this);
+}
+
+SpriteComponent::~SpriteComponent()
+{
+	mOwner->GetScene().GetRenderer()->RemoveSprite(this);
+}
+
+void SpriteComponent::SetTexture(const Texture& pTexture)
+{
+	mTexture = pTexture;
+	mTexture.UpdateInfo(mTexWidth, mTexHeight);
+}
+
+void SpriteComponent::Draw(Renderer& pRenderer)
+{
+	Vector2D origin{ mTexWidth / 2.0f, mTexHeight / 2.0f };
+	pRenderer.DrawSprite(*mOwner, mTexture, Rectangle(), origin, Renderer::Flip::None);
+}
---
+++ Engine-ARTFX/Core/Engine/Actor Components/SpriteComponent.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Component.h"
+#include "../Graphics/Texture.h"
+
+class SpriteComponent : public Component
+{
+public:
+	SpriteComponent(Actor* pOwner, Texture& pTexture, int pDrawOrder = 100);
+	virtual ~SpriteComponent();
+	SpriteComponent() = delete;
+	SpriteComponent(const SpriteComponent&) = delete;
+	SpriteComponent& operator= (const SpriteComponent&) = delete;
+
+	virtual void SetTexture(const Texture& pTexture);
+	virtual void Draw(Renderer& pRenderer);
+
+	int GetDrawOrder() const { return mDrawOrder; }
+	int GetTexWidth() const { return mTexWidth; }
+	int GetTexHeight() const { return mTexHeight; }
+protected:
+	Texture mTexture;
+	int mDrawOrder;
+	int mTexWidth;
+	int mTexHeight;
+private:
+
+};
+
