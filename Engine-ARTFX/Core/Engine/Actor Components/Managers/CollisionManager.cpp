aea753013b3d398a6605dc0d22dce830bba75046
Hettrick1 <evann.jacquot@gmail.com>
2025-02-19 13:57:56 +0100

    [fix] set hit result

--- Engine-ARTFX/Core/Engine/Actor Components/Managers/CollisionManager.cpp
+++ Engine-ARTFX/Core/Engine/Actor Components/Managers/CollisionManager.cpp
@@ -28,6 +28,7 @@ void CollisionManager::RegisterCollider(ColliderComponent* pCollider)
 
 void CollisionManager::CheckCollisions()
 {
+    // check only if the actor is active
     std::vector<ColliderComponent*> activeColliders;
     for (auto& collider : mAllColliders) {
         if (collider->GetOwner()->GetState() == ActorState::Active) {
@@ -37,21 +38,26 @@ void CollisionManager::CheckCollisions()
 
     std::unordered_map<ColliderComponent*, std::unordered_set<ColliderComponent*>> newCollisions;
 
+    // for each collisions check their state
     for (size_t i = 0; i < activeColliders.size() - 1; ++i) {
         for (size_t j = i + 1; j < activeColliders.size(); ++j) {
             auto* collider1 = activeColliders[i];
             auto* collider2 = activeColliders[j];
 
+            // check collision
             if (collider1->CheckCollisionWith(collider2)) {
+                // check if the collision is new to call anly one time the on trigger enter function
                 bool isNewCollision1 = mCurrentCollisions[collider1].find(collider2) == mCurrentCollisions[collider1].end();
                 bool isNewCollision2 = mCurrentCollisions[collider2].find(collider1) == mCurrentCollisions[collider2].end();
 
                 if (isNewCollision1) {
                     if (collider1->GetIsTriggerable()) {
+                        // set the hit result of the collision
                         collider1->SetHitResult(true, collider2->GetOwner());
                         collider1->NotifyListenersStarted();
                     }
                 }
+                // if not new call on trigger stay
                 else {
                     if (collider1->GetIsTriggerable()) {
                         collider1->NotifyListenersStay();
@@ -60,45 +66,56 @@ void CollisionManager::CheckCollisions()
 
                 if (isNewCollision2) {
                     if (collider2->GetIsTriggerable()) {
+                        // set the hit result of the collision
                         collider2->SetHitResult(true, collider1->GetOwner());
                         collider2->NotifyListenersStarted();
                     }
                 }
+                // if not new call on trigger stay
                 else {
                     if (collider2->GetIsTriggerable()) {
                         collider2->NotifyListenersStay();
                     }
                 }
 
+                // add the colliders to the new collision
                 newCollisions[collider1].insert(collider2);
                 newCollisions[collider2].insert(collider1);
             }
         }
     }
 
+    // if no collisions were detected
+    // check if the previous trigger collisions are ended
     std::unordered_set<std::pair<ColliderComponent*, ColliderComponent*>, CollisionPairHash> processedCollisions;
 
-    for (auto& pair : mCurrentCollisions) {
-        ColliderComponent* collider = pair.first;
-        std::unordered_set<ColliderComponent*>& previousCollisions = pair.second;
+    for (auto& pair : mCurrentCollisions) { 
+        ColliderComponent* collider = pair.first; 
+        std::unordered_set<ColliderComponent*>& previousCollisions = pair.second; 
+        for (auto* otherCollider : previousCollisions) 
+        { 
+            // if not in new collisions the collision was ended last frame
+            if (newCollisions[collider].find(otherCollider) == newCollisions[collider].end()) 
+            { 
+                std::pair<ColliderComponent*, ColliderComponent*> collisionPair = std::minmax(collider, otherCollider); 
 
-        for (auto* otherCollider : previousCollisions) {
-            if (newCollisions[collider].find(otherCollider) == newCollisions[collider].end()) {
-                std::pair<ColliderComponent*, ColliderComponent*> collisionPair =
-                    std::minmax(collider, otherCollider);
-
-                if (processedCollisions.find(collisionPair) == processedCollisions.end()) {
+                // check to call only one time the on trigger exit function
+                if (processedCollisions.find(collisionPair) == processedCollisions.end()) 
+                { 
                     processedCollisions.insert(collisionPair);
-
-                    if (collider->GetIsTriggerable()) {
-                        collider->NotifyListenersEnded();
+                    if (collider->GetIsTriggerable()) 
+                    { 
+                        collider->SetHitResult(true, otherCollider->GetOwner());
+                        collider->NotifyListenersEnded(); 
                     }
-                    if (otherCollider->GetIsTriggerable()) {
-                        otherCollider->NotifyListenersEnded();
+                    if (otherCollider->GetIsTriggerable()) 
+                    {
+                        otherCollider->SetHitResult(true, collider->GetOwner());
+                        otherCollider->NotifyListenersEnded(); 
                     }
-
-                    collider->SetHitResult(false, nullptr);
-                    otherCollider->SetHitResult(false, nullptr);
+                    // reset hit result
+                    collider->SetHitResult(false, nullptr); 
+                    otherCollider->SetHitResult(false, nullptr); 
                 }
             }
         }
