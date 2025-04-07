f6870a49803d26607b044713e6f5f87ecf23a8eb
Evann Jacquot <evann.jacquot@artfx.fr>
2025-04-07 10:47:56 +0200

    [add] basic cube actor

---
+++ Engine-ARTFX/Core/Engine/CoreActors/BasicCube.cpp
@@ -0,0 +1,51 @@
+#include "BasicCube.h"
+#include "Mesh.h"
+#include "MeshComponent.h"
+#include "Assets.h"
+#include "Scene.h"
+#include "BoxCollider3DComponent.h"
+
+BasicCube::BasicCube(Vector3D pPos, Vector3D pSize, Quaternion pRotation, ShaderProgram* program)
+	: mShaderProgram(program)
+{
+}
+
+BasicCube::~BasicCube()
+{
+}
+
+void BasicCube::Start()
+{
+	Actor::Start();
+	Texture* tex = Assets::LoadTexture(*GetScene().GetRenderer(), "Imports/Sprites/planks.png", "cube");
+	Mesh* mesh = Assets::LoadMesh("Imports/Meshes/cube.obj", "cube");
+	mesh->AddTexture(tex);
+	if (mShaderProgram != nullptr) {
+		mesh->SetShaderProgram(*mShaderProgram);
+	}
+	MeshComponent* meshComp = new MeshComponent(this, mesh);
+	meshComp->SetTextureIndex(0);
+	BoxCollider3DComponent* bc = new BoxCollider3DComponent(this, 10, GetTransformComponent().GetSize());
+}
+
+void BasicCube::Update()
+{
+	Actor::Update();
+}
+
+void BasicCube::Destroy()
+{
+	Actor::Destroy();
+}
+
+void BasicCube::OnTriggerEnter(ColliderComponent* collider, HitResult* infos)
+{
+}
+
+void BasicCube::OnTriggerStay(ColliderComponent* collider, HitResult* infos)
+{
+}
+
+void BasicCube::OnTriggerExit(ColliderComponent* collider, HitResult* infos)
+{
+}
---
+++ Engine-ARTFX/Core/Scenes/Childs/LVLAdvancedOpenGL.cpp
@@ -0,0 +1,29 @@
+#include "LVLAdvancedOpenGL.h"
+
+LVLAdvancedOpenGl::LVLAdvancedOpenGl()
+{
+}
+
+LVLAdvancedOpenGl::~LVLAdvancedOpenGl()
+{
+}
+
+void LVLAdvancedOpenGl::Start(IRenderer* renderer)
+{
+	Scene::Start(renderer);
+}
+
+void LVLAdvancedOpenGl::Update()
+{
+	Scene::Update();
+}
+
+void LVLAdvancedOpenGl::Render()
+{
+	Scene::Render();
+}
+
+void LVLAdvancedOpenGl::Close()
+{
+	Scene::Close();
+}
